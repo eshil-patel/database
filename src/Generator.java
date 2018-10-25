73f678e9dd7251a86fcdd69b86702f2c4ede4c2d
Eshil Patel <xeshil101@optonline.net>
2018-10-25 19:20:47 -0400

    Update Generator.java

--- src/Generator.java
+++ src/Generator.java
@@ -17,6 +17,10 @@ public class Generator {
 	
 	public static String[] phoneNumbers = new String[500];
 	
+	public static String[] times = {"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00",
+					"12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"}
+		
+	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		File file = new File("Names.txt");
