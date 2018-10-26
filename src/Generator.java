53e3142c12c56156d432ec39dc1900338694ed62
Masood Karimi <masoodkkarimi@gmail.com>
2018-10-26 12:32:19 -0400

    Bars.csv added

--- src/Generator.java
+++ src/Generator.java
@@ -28,8 +28,13 @@ public class Generator {
 	public static String[] times = {"00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00",
 					"12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"};
 		
+	public static String[] openTimes = { "12:00","13:00","14:00","15:00" };
 	
-	@SuppressWarnings("resource")
+	public static String[] closeTimes = { "01:00","02:00","03:00","04:00" };
+	public static String[] licenses = new String[100];
+	public static String[][] barAddresses = new String[100][3];
+	public static String[] barPhoneNumbers = new String[100];
+
 	public static void main(String[] args) {
 		File file = new File("Names.txt");
 		try {
@@ -55,6 +60,19 @@ public class Generator {
 			
 			sc.close();
 			
+			sc = new Scanner(new File("bar_address.txt"));
+			i = 0;
+			while(sc.hasNextLine()) {
+				String street = sc.nextLine().trim();
+				String data = sc.nextLine();
+				String city = data.split(",")[0];
+				String state = data.split(",")[1].split(" ")[1];
+				barAddresses[i] = new String[] { street, city, state };
+				i++;
+			}
+			
+			sc.close();
+			
 			sc=new Scanner(new File("bar_name.txt"));
 			int j=0;
 			while(sc.hasNextLine()) 
@@ -72,16 +90,64 @@ public class Generator {
 				phoneNumbers[i] = sc.nextLine();
 				i++;
 			}
+			sc.close();
+			
+			sc = new Scanner(new File("bar_phone_number.txt"));
+			i = 0;
+			while(sc.hasNextLine()) {
+				barPhoneNumbers[i] = sc.nextLine();
+				i++;
+			}
+			sc.close();
+			
+			sc = new Scanner(new File("license"));
+			i = 0;
+			while(sc.hasNextLine()) {
+				licenses[i++] = sc.nextLine();
+			}
+			sc.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 		}
 	
-		generateDrinkerCSV();
+		//generateDrinkerCSV();
+		generateBarCSV();
 //		for (int i = 0; i < 500; i++) {
 //			System.out.println(addresses[i][0] + addresses[i][1] + addresses[i][2]);
 //		}
 	}
 	
+	/**
+	 * Generate Bars CSV
+	 */
+	public static void generateBarCSV() {
+		try {
+			PrintWriter writer = new PrintWriter(new File("bars.csv"));
+			StringBuilder sb = new StringBuilder();
+			sb.append("barId").append(',').append("barName").append(',').append("license").append(',')
+			.append("open").append(',').append("close").append(',').append("phone")
+			.append(',').append("address").append(',').append("city").append(',').append("state").append('\n');
+			
+			
+			for (int i = 0; i < 100; i++) {
+				String open = openTimes[RANDOM.nextInt(openTimes.length)];
+				String close = closeTimes[RANDOM.nextInt(closeTimes.length)];
+				String[] data = barAddresses[i];
+				sb.append("" + i).append(',').append(bar_name[i]).append(',').append(data[2] + licenses[i]).append(',')
+				.append(open).append(',').append(close).append(',').append(barPhoneNumbers[i])
+				.append(',').append(data[0]).append(',').append(data[1]).append(',').append(data[2]).append('\n');
+			}
+			writer.write(sb.toString());
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Generates Drinker CSV.
+	 */
 	public static void generateDrinkerCSV() {
 		try {
 			PrintWriter writer = new PrintWriter(new File("drinkers.csv"));
